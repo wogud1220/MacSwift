fd056763d9232a5fdb16b8a3cc9c395e47f89c83
wogud1220 <https://github.com/wogud1220>
2023-10-11 22:54:59 +0900

    asdf

--- test.playground/Contents.swift
+++ test.playground/Contents.swift
@@ -1,4 +1,19 @@
-import UIKit
+let val1: Int = 3
+let val2: Int = 4
+var result: Int
+var a: Int = 3;
+if val1 > val2 {
+    result = val1
+}
+else if val1 < val2 {
+result = val2
+}
+else if val1 == val2 { result = 0
+}
+else {
+result = -1
 
-var greeting = "Hello, playground"
-print(greeting)
+
+
+}
+print(result)
