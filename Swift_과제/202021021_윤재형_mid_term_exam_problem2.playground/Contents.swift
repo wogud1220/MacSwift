76c293d68d33ec74963fe77ae6b134de193b5983
wogud1220 <https://github.com/wogud1220>
2023-10-26 10:31:57 +0900

    mid

--- Swift_과제/202021021_윤재형_mid_term_exam_problem2.playground/Contents.swift
+++ Swift_과제/202021021_윤재형_mid_term_exam_problem2.playground/Contents.swift
@@ -4,21 +4,21 @@ struct CoordinatePoint {
     var x: Double = 0
     var y: Double = 0
 }
-class Position {
+class Position {
     var name: String? = nil
     var point: CoordinatePoint? = nil
-    // 타입 프로퍼티인 instanceCount를 선언하기 위해 line 12에서 ???로 표시된 부분 구현
+    // ?? ????? instanceCount? ???? ?? line 12?? ???? ??? ?? ??
     static var instanceCount: Int = 0
-    // Position 클래스의 생성자(initializer)를 정의하기 위해 line 15에서 ???로 표시된 부분 구현
+    // Position ???? ???(initializer)? ???? ?? line 15?? ???? ??? ?? ??
     init?(name: String?, point: CoordinatePoint?){
         if let nm = name, let pt = point {
             if nm.isEmpty || pt.x < 0 || pt.y < 0 {
                 print("Instance is not generated!")
                 return nil
             } else {
-                // Position 클래스의 생성자 내에서 reset 메소드를 호출하기 위해 line 22에서 ???로 표시된 부분 구현
+                // Position ???? ??? ??? reset ???? ???? ?? line 22?? ???? ??? ?? ??
                 self.reset(newName: nm, newPoint: pt)
-                // 타입 프로퍼티인 instanceCount는 초기에 0의 값을 가지며 Position 클래스의 인스턴스가 생성될 때마다 1씩 증가되도록 line 24에서 ???로 표시된 부분 구현
+                // ?? ????? instanceCount? ??? 0? ?? ??? Position ???? ????? ??? ??? 1? ????? line 24?? ???? ??? ?? ??
                 Self.instanceCount = Self.instanceCount + 1    //self.instanceCount
             }
         } else {
@@ -43,7 +43,7 @@ class Position {
     }
 
     func printNameAndPoint() {
-        // Position 클래스의 인스턴스의 name과 point 프로퍼티를 화면에 출력하기 위해 line 49에서 ???로 표시된 부분 구현
+        // Position ???? ????? name? point ????? ??? ???? ?? line 49?? ???? ??? ?? ??
         if let nm = self.name, let pt = self.point {
             print("name: \(nm)")
             print("position: (\(pt.x), \(pt.y))")
@@ -57,19 +57,14 @@ class Position {
             print("Number of generated instances: \(Position.instanceCount)")
         } else {
             print("instanceCount is equal to nil")
-    }
+        }
+    }  
 }
-
-/*if Position.instanceCount > 0 {
-            print("Number of generated instances: \(Position.instanceCount)")
-        } else {
-            print("instanceCount is equal to nil")
-        }*/
 
 var pointA: CoordinatePoint = CoordinatePoint(x: 2.3, y: 3.4)
 var pointB: CoordinatePoint = CoordinatePoint(x: 4.5, y: 5.6)
 
-// Position 클래스의 인스턴스의 생성하기 위해 line 71, 72, 73에서 ???로 표시된 부분 구현
+// Position ???? ????? ???? ?? line 71, 72, 73?? ???? ??? ?? ??
 let nobody: Position? = Position(name: nil, point: nil)
 let kim: Position? = Position(name: "Kim", point: pointA)
 let lee: Position? = Position(name: "Kim", point: pointA)
@@ -83,5 +78,5 @@ if let point = lee?.accessPoint {
     print("Lee's position: (\(point.x), \(point.y))")
 }
 
-// printInstanceCount 메소드를 호출하기 위해 line 85에서 ???로 표시된 부분 구현
+// printInstanceCount ???? ???? ?? line 85?? ???? ??? ?? ??
 Position.printInstanceCount()
