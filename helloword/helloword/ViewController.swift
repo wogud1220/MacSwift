ba7e6453fe971880269a2fced933d3a631358ca8
wogud1220 <https://github.com/wogud1220>
2023-11-03 16:47:16 +0900

    ㄹㄹ

--- helloword/helloword/ViewController.swift
+++ helloword/helloword/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var lblHello: UILabel!
+    @IBOutlet var txtName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnsend(_ sender: UIButton) {
+        lblHello.text = "Hello, " + txtName.text!
+        
+    }
+    
 }
 
