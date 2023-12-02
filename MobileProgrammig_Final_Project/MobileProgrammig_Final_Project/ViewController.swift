0e391381b4519b2ef6d0e261dcaede493fc7be0b
wogud1220 <https://github.com/wogud1220>
2023-12-02 20:08:41 +0900

    asdf

--- MobileProgrammig_Final_Project/MobileProgrammig_Final_Project/ViewController.swift
+++ MobileProgrammig_Final_Project/MobileProgrammig_Final_Project/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var sickLabel: UILabel!
+    var humanImg: UIImage?
+    @IBOutlet var imgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        humanImg = UIImage(named: "sumnail_250.png")
+        imgView.image = humanImg //이미지 뷰에 켜진 이미지 넣음
     }
 
 
 }
+
+
 
--- Swift_과제/test4.playground/Contents.swift
+++ Swift_과제/test4.playground/Contents.swift
@@ -1,18 +1,66 @@
+import AVFoundation
 
-// 1) 구조체
-struct CoordinatePoint {
-var x:Int=0 
-var y:Int=0
-}
-
-/*var kimPoint: CoordinatePoint = CoordinatePoint() 또는
-var kimPoint: CoordinatePoint
-kimPoint = CoordinatePoint()
-
-kimPoint.x = 10
-kimPoint.y = 20
-*/
-
-let kimPoint: CoordinatePoint = CoordinatePoint()
-kimPoint.x = 10 
-kimPoint.y = 20 
+class C_TTS {
+    
+    
+    /*
+     // -----------------------------
+     [요약 설명]
+     // -----------------------------
+     1. Text To Speech : 문자열을 음성으로 읽어주는 것 입니다
+     // -----------------------------
+     2. 필요 import 설정 :
+     
+     import AVFoundation
+     // -----------------------------
+     3. 사용 방법 :
+     
+     C_TTS.shared.play("안녕하세요")
+     
+     C_TTS.shared.stop()
+     // -----------------------------
+     4. 테스트 : 실제 디바이스 기기 연결 및 TTS 코드 호출 실시
+     // -----------------------------
+     */
+    
+    
+    // [변수 선언 실시]
+    let ACTIVITY_NAME = "C_TTS"
+    
+    
+    // [전역 싱글톤 객체 선언 실시]
+    static let shared = C_TTS()
+    private let synthesizer = AVSpeechSynthesizer()
+    
+    
+    // [TTS 시작]
+    internal func play(_ string: String) {
+        print("")
+        print("====================================")
+        print("[\(self.ACTIVITY_NAME) >> play() :: TTS 음성 재생 실시]")
+        print("string :: \(string)")
+        print("====================================")
+        print("")
+        
+        let utterance = AVSpeechUtterance(string: string) // [음성 텍스트]
+        utterance.voice = AVSpeechSynthesisVoice(language: "ko-KR") // [음성 언어 발음]
+        utterance.rate = 0.4 // [음성 속도]
+        synthesizer.stopSpeaking(at: .immediate) // [음성 실행 즉시 중지]
+        synthesizer.speak(utterance) // [음성 실행]
+    }
+    
+    
+    // [TTS 종료]
+    internal func stop() {
+        print("")
+        print("====================================")
+        print("[\(self.ACTIVITY_NAME) >> stop() :: TTS 음성 재생 중지]")
+        print("====================================")
+        print("")
+        
+        // [음성 실행 즉시 중지]
+        synthesizer.stopSpeaking(at: .immediate)
+    }
+    
+    
+} // [클래스 종료]
